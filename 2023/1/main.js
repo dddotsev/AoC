12129dd7aae90f8cbfb005ef3e488c5433dc309b
Daniil Dotsev <daniil.dotsev@scalefocus.com>
2023-12-01 18:51:42 +0200

    Add Day 1 Task 2

--- 2023/1/main.js
+++ 2023/1/main.js
@@ -1,16 +1,17 @@
 import fs from 'fs';
-  
-// Use fs.readFile() method to read the file 
-fs.readFile('input.txt', 'utf8', function(err, data){ 
-      
+
+const main = () => {
+    const data = fs.readFileSync('input.txt', 'utf8');
     const lines = data.split(/\r?\n/);
 
-    const sum = lines.reduce((sum, cur) => sum + getNum(cur), 0);
+    const sum1 = lines.reduce((sum, cur) => sum + getNumberFromDigits(cur), 0);
+    console.log(sum1);
 
-    console.log(sum);
-});
+    const sum2 = lines.reduce((sum, cur) => sum + getNumberFromDigitsAndLetters(cur), 0);
+    console.log(sum2);
+};
 
-const getNum = line => {
+const getNumberFromDigits = line => {
     const chars = Array.from(line);
     let first, last;
 
@@ -27,7 +28,40 @@ const getNum = line => {
 
     return first * 10 + last;
 };
-
 const isNumber = c => {
     return /^\d$/.test(c);
 }
+
+
+const numbers =   ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+const numberMap = [1,      2,     3,       4,      5,      6,     7,       8,       9,      0,   1,   2,   3,   4,   5,   6,   7,   8,   9 ];
+
+const getNumberFromDigitsAndLetters = line => {
+    const chars = Array.from(line);
+    let first, last;
+    const matchedTos = new Array(numbers.length).fill(-1);
+
+    for (const c of chars) {
+        for (let i = 0; i < numbers.length; i++) {
+            const number = numbers[i];
+
+            if (c === number[matchedTos[i]+1]) {
+                matchedTos[i]++;
+                if (matchedTos[i] + 1 === number.length) {
+                    last = numberMap[i];
+                    first = first === undefined ? numberMap[i] : first;
+                }
+            } else {
+                // since there are no 2 matching common sequences of letters in the numbers, it's ok to just check the first letter
+                matchedTos[i] = c === number[0] ? 0 : -1;
+            }
+        }
+    }
+
+    first = first || 0;
+    last = last || 0;
+
+    return first * 10 + last;
+};
+
+main();
